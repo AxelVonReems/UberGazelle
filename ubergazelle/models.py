31caa1d878356a9684602b1105c4d6867921e645
axelvonreems <axelvonreems@gmail.com>
2023-06-17 15:15:04 +0200

    Создана новая таблица для регионов со связью O2M к региону модели Driver

--- ubergazelle/models.py
+++ ubergazelle/models.py
@@ -1,20 +1,35 @@
 from flask_sqlalchemy import SQLAlchemy
 
-from .regions import Regions
 from .vehicle_types import VehicleTypes
 
 db = SQLAlchemy()
 
 
+class RegionNames(db.Model):
+    __tablename__ = 'Region names'
+    id = db.Column(db.Integer, primary_key=True)
+    region_name = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f'<ID региона {self.id}, название региона {self.region_name}>'
+
+
 class Client(db.Model):
+    __tablename__ = 'Clients'
     id = db.Column(db.Integer, primary_key=True)
     client_telegram_id = db.Column(db.Integer, unique=True)
     weight = db.Column(db.Integer, nullable=False)
     width = db.Column(db.Integer, nullable=False)
     height = db.Column(db.Integer, nullable=False)
-    pickup_region = db.Column(db.Enum(Regions), nullable=False)
+    pickup_region = db.Column(
+        db.String, db.ForeignKey(RegionNames.region_name),
+        nullable=False, unique=True
+    )
     pickup_address = db.Column(db.Text, nullable=False)
-    delivery_region = db.Column(db.Enum(Regions), nullable=False)
+    delivery_region = db.Column(
+        db.String, db.ForeignKey(RegionNames.region_name),
+        nullable=False, unique=True
+    )
     delivery_address = db.Column(db.Text, nullable=False)
     vehicle_type = db.Column(db.Enum(VehicleTypes), nullable=False)
     description = db.Column(db.Text, nullable=True)
@@ -29,9 +44,12 @@ class Client(db.Model):
 
 
 class Driver(db.Model):
+    __tablename__ = 'Drivers'
     id = db.Column(db.Integer, primary_key=True)
     driver_telegram_id = db.Column(db.Integer, unique=True)
-    work_regions = db.Column(db.Enum(Regions), nullable=False)
+    work_regions = db.Column(
+        db.String, db.ForeignKey(RegionNames.region_name), nullable=False
+        )
     vehicle_type = db.Column(db.Enum(VehicleTypes), nullable=False)
 
     def __repr__(self):
